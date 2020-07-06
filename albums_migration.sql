96597f4daac4942ea51447e1881ed56f938bac4e
Shelby Polasek <shelbypolasek@gmail.com>
2020-07-06 16:11:49 -0500

    walkthrough

--- albums_migration.sql
+++ albums_migration.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS albums(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist_name VARCHAR(20) NOT NULL,
     record_name VARCHAR(50) NOT NULL,
-    release_date DATE NOT NULL,
+    release_date SMALLINT  UNSIGNED,
     sales FLOAT UNSIGNED NOT NULL,
     genre VARCHAR(20) DEFAULT 'NONE',
     PRIMARY KEY(id)
