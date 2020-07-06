e4b48dd3260e01ad6719a9b376fb6781e0c7ec68
Shelby Polasek <shelbypolasek@gmail.com>
2020-07-06 13:54:59 -0500

    connected albums mysql

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,13 +1,14 @@
+
 USE codeup_test_db;
 
-DROP TABLE IF EXITS albums
+DROP TABLE IF EXISTS albums;
 
 CREATE TABLE IF NOT EXISTS albums(
-    id INT UNSIGNED AUTO-INCREMENT,
-    artist VARCHAR(20),
-    name VARCHAR(50),
-    release_date DATE,
-    sales FLOAT UNSIGNED,
-    genre VARCHAR(20),
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist_name VARCHAR(20) NOT NULL,
+    record_name VARCHAR(50) NOT NULL,
+    release_date DATE NOT NULL,
+    sales FLOAT UNSIGNED NOT NULL,
+    genre VARCHAR(20) DEFAULT 'NONE',
     PRIMARY KEY(id)
-)
+);
