9a265349e755a193449b8cf97132ca27a173943c
Shelby Polasek <shelbypolasek@gmail.com>
2020-07-07 12:19:38 -0500

    finished basic statments

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -8,7 +8,7 @@ VALUES
     ('AC/DC', 'Back in Black', 1980,	'Hard rock', 26.1),
     ('Pink Floyd',	'The Dark Side of the Moon', 1973, 'Progressive rock', 24.2),
     ('Meat Loaf', 'Bat Out of Hell',	1977, 'Hard rock/Progressive rock', 21.5),
-    ('Whitney Houston / Various artists',	'The Bodyguard',	1992,	'R&B/Soul/Pop/Soundtrack',	28.4),
+    ('Whitney Houston',	'The Bodyguard',	1992,	'R&B/Soul/Pop/Soundtrack',	28.4),
     ('Eagles', 'Their Greatest Hits', 1976,	'Rock/Soft', 32.2),
     ('Bee Gees / Various artists',	'Saturday Night Fever',	1977,	'Disco',	21.6),
     ('Fleetwood Mac',	'Rumours',	1977,	'Soft rock',	27.9),
---
+++ delete_exercises.sql
@@ -0,0 +1,15 @@
+
+--  4-a
+SELECT 'Delete albums after 1991' AS 'Exercise 4-a'
+SELECT * FROM albums WHERE release_date > 1991;
+DELETE FROM albums WHERE release_date > 1991;
+
+-- 4-b
+SELECT 'Delete disco genre' AS 'Exercise 4-b'
+SELECT * FROM albums WHERE genre = 'disco';
+DELETE FROM albums WHERE genre = 'disco';
+
+-- 4-c
+SELECT 'Delete Whitney Houston' AS 'Exercise 4-c'
+SELECT * FROM albums WHERE artist_name LIKE '%Whitney Houston%';
+DELETE FROM albums WHERE artist_name LIKE '%Whitney Houston%';
