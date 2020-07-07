f1e611c106f67880d5ed09d0bcd3345b10ff097f
Shelby Polasek <shelbypolasek@gmail.com>
2020-07-07 12:01:57 -0500

    finished part 3

---
+++ update_exercises.sql
@@ -0,0 +1,19 @@
+USE codeup_test_db;
+
+-- 2-a
+SELECT 'All albums' AS 'Exercise 2-a';
+SELECT * FROM albums;
+UPDATE albums SET sales = (sales * 10);
+SELECT sales FROM albums;
+
+-- 2-b
+SELECT 'All albums release before 1980' AS 'Exercise 2-b';
+SELECT * FROM albums WHERE release_date < 1980;
+UPDATE albums SET release_date = (release_date + 100) WHERE release_date < 1980;
+SELECT * FROM albums WHERE release_date < 1980;
+
+-- 2-c
+SELECT 'Jackson albums' AS 'Exercise 2-c';
+SELECT * FROM albums WHERE artist_name = 'Michael Jackson';
+UPDATE albums SET artist_name = 'Peter Jackson' WHERE artist_name = 'Michael Jackson';
+SELECT * FROM albums WHERE artist_name = 'Peter Jackson';
