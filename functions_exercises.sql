2c98e905ef8cd0a0db3b0c11b51f0938ec54082d
Shelby Polasek <shelbypolasek@gmail.com>
2020-07-08 11:32:26 -0500

    finished function exercise

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -8,7 +8,7 @@ WHERE last_name LIKE 'E%'
   AND last_name LIKE '%E';
 
 #3
-SELECT first_name, last_name, DATEDIFF(CURDATE(), hire_date)
+SELECT first_name, last_name, hire_date, curdate() ,DATEDIFF(CURDATE(), hire_date) AS days_since_hired
 FROM employees
 WHERE birth_date LIKE '%12-25'
   AND hire_date BETWEEN '1990-01-01' AND '1999-12-31'
